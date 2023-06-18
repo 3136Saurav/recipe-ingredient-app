5f68848c501874b7fb3a58e4ebd523833ea318cc
Saurav Rathore <saurav_rathore@epam.com>
2023-06-18 20:36:15 +0530

    Added Authentication and Route Protection in Angular

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+import { Subscription } from "rxjs";
 
 
 @Component({
     templateUrl: './header.component.html',
     selector: 'app-header'
 })
-export class HeaderComponent {
-    constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {  
+    private userSub: Subscription 
+    isAuthenticated: boolean = false;
+
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+   
+    ngOnInit(): void {
+        this.userSub = this.authService.user.subscribe((user) => {
+            this.isAuthenticated = !user ? false : true
+            console.log(user);
+        })
+    }
 
     onSaveData() {
         this.dataStorageService.storeRecipes()
@@ -15,5 +27,13 @@ export class HeaderComponent {
 
     onFetchData() {
         this.dataStorageService.fetchRecipes().subscribe()
+    }
+
+    onLogout() {
+        this.authService.logout()
+    }
+
+    ngOnDestroy(): void {
+        this.userSub.unsubscribe()
     }
 }
